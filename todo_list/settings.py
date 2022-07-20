39cf0d2feec23937403358f69dba9cb699b05753
Javier Azanza <azzanza@gmail.com>
2022-07-19 22:20:58 -0500

    dockerize project

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-+^w#-08a0^_r&$aor@uc5m)omw=(41jiqjpwcao&#r2ikk)6s-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
